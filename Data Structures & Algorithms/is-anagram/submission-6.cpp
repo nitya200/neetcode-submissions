085093124604f0fd404fd49a98821f7b1f3bf9d7
CHIMMILI NITYANANDH <23P31A05K5@acet.ac.in>
2026-05-14 20:40:28 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        vector<int>arr(26,0);
+        for(int i=0;i<s.length();i++){
+           arr[s[i]-'a']++;
+           arr[t[i]-'a']--;
+        }
+        for(auto val : arr){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
