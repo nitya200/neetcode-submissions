8a6d91d1ca3eee014c9c983d98b8ebafc432f409
CHIMMILI NITYANANDH <23P31A05K5@acet.ac.in>
2026-06-04 21:17:31 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.length();
+        vector<int>freq(26,0);
+        int left=0;
+        int ans=0;
+        int maxfreq=0;
+        for(int right=0;right<n;right++){
+                freq[s[right]-'A']++;
+                maxfreq=max(maxfreq,freq[s[right]-'A']);
+                if((right-left+1)-maxfreq > k){
+                        freq[s[left]-'A']--;
+                        left++;
+                }
+                ans=max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
