cda6d12bf92f479a7e3e1d7bec9d936b14ef0cfc
CHIMMILI NITYANANDH <23P31A05K5@acet.ac.in>
2026-07-16 14:55:21 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1,mid=0;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]< target){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        return low;
+    }
+};
